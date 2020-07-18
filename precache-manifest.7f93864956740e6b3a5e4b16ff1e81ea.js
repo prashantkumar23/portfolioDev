2f9f6fecb6fc356f2fa9d46e4166e53465068463
Prashant <blackhole.gamma@gmail.com>
2020-07-18 19:35:13 +0530

    Updates

---
+++ precache-manifest.7f93864956740e6b3a5e4b16ff1e81ea.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4869dceb60c5322e90681a8fb17f3f64",
+    "url": "/portfolioDev/index.html"
+  },
+  {
+    "revision": "2f0b14d42e5cb8621c90",
+    "url": "/portfolioDev/static/js/2.8c3e0bac.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/portfolioDev/static/js/2.8c3e0bac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2dc79d4933c1b4f33106",
+    "url": "/portfolioDev/static/js/main.045b7620.chunk.js"
+  },
+  {
+    "revision": "7365fb1e91267bd6cce6",
+    "url": "/portfolioDev/static/js/runtime-main.8829d6e3.js"
+  },
+  {
+    "revision": "3707da3c6a394c7ee2523273f8d98a25",
+    "url": "/portfolioDev/static/media/css3.3707da3c.svg"
+  },
+  {
+    "revision": "62f80bab2d703a54fde6787cddede42c",
+    "url": "/portfolioDev/static/media/git.62f80bab.svg"
+  },
+  {
+    "revision": "32bc428f67abce6b369e0d6cf224f28a",
+    "url": "/portfolioDev/static/media/graphql.32bc428f.svg"
+  },
+  {
+    "revision": "9f2e1637c83e37e8f555044e12324734",
+    "url": "/portfolioDev/static/media/html5.9f2e1637.svg"
+  },
+  {
+    "revision": "61ed8b054178d0e8d1285ad8f29f4045",
+    "url": "/portfolioDev/static/media/javascript.61ed8b05.svg"
+  },
+  {
+    "revision": "2fadd2a137085081faa798150fee4a62",
+    "url": "/portfolioDev/static/media/materialui.2fadd2a1.svg"
+  },
+  {
+    "revision": "554c7d4bfd63a3da33773978fd291c06",
+    "url": "/portfolioDev/static/media/mongodb.554c7d4b.svg"
+  },
+  {
+    "revision": "5c376133d6403088a87a04fcc9685674",
+    "url": "/portfolioDev/static/media/nodejs.5c376133.svg"
+  },
+  {
+    "revision": "dfef6e8181b01d7dae3fbbb687367c3a",
+    "url": "/portfolioDev/static/media/profileImage.dfef6e81.svg"
+  },
+  {
+    "revision": "5986ef75123f70ab4148c534d0c5ede4",
+    "url": "/portfolioDev/static/media/project.5986ef75.svg"
+  },
+  {
+    "revision": "7038477259b6f1696308f867ce8c0a3a",
+    "url": "/portfolioDev/static/media/reactjs.70384772.svg"
+  },
+  {
+    "revision": "b4477375cbcd42acb9cb627611390dbf",
+    "url": "/portfolioDev/static/media/redis.b4477375.svg"
+  },
+  {
+    "revision": "aa0a337603611296524a666b62a9ff23",
+    "url": "/portfolioDev/static/media/redux.aa0a3376.svg"
+  },
+  {
+    "revision": "ce0fd9cb13a3e63c5b680c9579d47df5",
+    "url": "/portfolioDev/static/media/sql.ce0fd9cb.svg"
+  },
+  {
+    "revision": "3aeee2292aa7770ccf0aec8e43bdaaf5",
+    "url": "/portfolioDev/static/media/typescript.3aeee229.svg"
+  },
+  {
+    "revision": "2576f9679bfe0ec98db2ab1a4362566b",
+    "url": "/portfolioDev/static/media/user.2576f967.svg"
+  }
+]);
